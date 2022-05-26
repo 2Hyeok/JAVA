3e128c766ee3c61d953e72d9feb3a2b0b396cc0f
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-26 19:53:42 +0900

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,11 @@
+package Vehicle;
+
+public class Bus implements Vehicle{
+	@Override
+	public void run() {
+		System.out.println("버스가 달립니다.");
+	}
+	public void checkFare() {
+		System.out.println("승차요금을 체크함");
+	}
+}

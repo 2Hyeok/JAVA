945a6268268195227820c33e22df450249463fd2
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 18:45:41 +0900

    Add files via upload

---
+++ day5/createthread/BeepPrintExample2.java
@@ -0,0 +1,37 @@
+package createthread;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample2 {
+	public static void main(String[] args) {
+		
+		Runnable beepTask = new BeepTask();
+		Thread thread = new Thread(beepTask);
+		
+		/*Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();	
+				for(int i=0; i<5; i++) {		
+					toolkit.beep();
+					try { Thread.sleep(500); } catch(Exception e) {}
+				}
+			}
+		});*/
+		
+		/*Thread thread = new Thread(() -> {
+			Toolkit toolkit = Toolkit.getDefaultToolkit();	
+			for(int i=0; i<5; i++) {		
+				toolkit.beep();
+				try { Thread.sleep(500); } catch(Exception e) {}
+			}
+		});*/
+		
+		thread.start(); // BeepTask, Execute run(), method, 그리고 동시에 아래 "띵" 출력
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("띵");
+			try { Thread.sleep(500); } catch(Exception e) {}
+		}
+	}
+}

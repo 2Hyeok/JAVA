7bc2c6138c24a7a58ce275c9a486279885812d55
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 13:24:02 +0900

    Rename PolymorEx03.java to day2/PolymorEx03.java

---
+++ day2/PolymorEx03.java
@@ -0,0 +1,23 @@
+// 메소드 오버라이딩
+class Weapon {
+	protected int fire() {
+		return 1; // 무기는 기본적으로 한 명만 살상
+	}
+}
+class Cannon extends Weapon {
+	@Override
+	protected int fire() { // 오버라이딩
+		return 10; // 대포는 한 번에 10명을 살상
+	}
+}
+
+public class PolymorEx03 {
+
+	public static void main(String[] args) {
+		Weapon weapon;
+		weapon = new Weapon(); // Weapon w = new Weapon();
+		System.out.println("기본 무기의 살상 능력은 " + weapon.fire());
+		weapon = new Cannon();
+		System.out.println("대포의 살상 능력은 " + weapon.fire());
+	}
+}

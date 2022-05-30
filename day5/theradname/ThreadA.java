e36b60d411d525aa0ebcef8cf990cb904765cd39
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 19:01:56 +0900

    Update and rename day5/ThreadA.java to day5/theradname/ThreadA.java

--- day5/theradname/ThreadA.java
+++ day5/theradname/ThreadA.java
@@ -7,7 +7,7 @@ public class ThreadA extends Thread {
 	
 	public void run() {
 		for(int i=0; i<2; i++) {
-			System.out.println(getName() + "°¡ Ãâ·ÂÇÑ ³»¿ë");
+			System.out.println(getName() + "ê°€ ì¶œë ¥í•œë‚´ìš©");
 		}
 	}
 }

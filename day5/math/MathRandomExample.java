a91e69b6ab5f7b32cfd7e40eadee64ede816f463
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 18:56:29 +0900

    Add files via upload

---
+++ day5/math/MathRandomExample.java
@@ -0,0 +1,10 @@
+package math;
+
+public class MathRandomExample {
+	public static void main(String[] args) {
+		for (int i = 1; i<=2; i++) {
+		int num = (int) (Math.random()*6) + 1;
+		System.out.println("ÁÖ»çÀ§ ´«: " + num);
+		}
+	}
+}

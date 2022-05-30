c5c64b6f376ce7b824b4402f82ec716c6d70fd64
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 18:40:44 +0900

    Rename day5/ArrayCopyExample.java to day5/arrays/ArrayCopyExample.java

--- day5/arrays/ArrayCopyExample.java
+++ day5/arrays/ArrayCopyExample.java
@@ -6,15 +6,15 @@ public class ArrayCopyExample {
 	public static void main(String[] args) {
 		char[] arr1 = {'J', 'A', 'V', 'A'};
 		
-		//¹æ¹ý1
+		//Â¹Ã¦Â¹Ã½1
 		char[] arr2 = Arrays.copyOf(arr1, arr1.length);
 		System.out.println(Arrays.toString(arr2));
 		
-		//¹æ¹ý2
+		//Â¹Ã¦Â¹Ã½2
 		char[] arr3 = Arrays.copyOfRange(arr1, 1, 3);
 		System.out.println(Arrays.toString(arr3));		
 		
-		//¹æ¹ý3
+		//Â¹Ã¦Â¹Ã½3
 		char[] arr4 = new char[arr1.length];
 		System.arraycopy(arr1, 0, arr4, 0, arr1.length);
 		for(int i=0; i<arr4.length; i++) {

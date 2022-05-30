778c341e5c6af1f185e0667a31dd26441f7f9156
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 18:43:27 +0900

    Add files via upload

---
+++ day5/clone/MemberExample.java
@@ -0,0 +1,12 @@
+package clone;
+
+import java.util.Date;
+
+public class MemberExample {
+	public static void maon(String[] args) {
+		Object obj1 = new Object();
+		Date obj2 = new Date();
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+	}
+}

e5778f1351edf0604394d2ce83e353d814cfc539
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 19:04:21 +0900

    Add files via upload

---
+++ day5/toString/SmartPhoneExample.java
@@ -0,0 +1,11 @@
+package toString;
+public class SmartPhoneExample {
+	public static void main(String[] args) {
+		SmartPhone myPhone = new SmartPhone("구글", "안드로이드");
+		
+		String strObj = myPhone.toString();
+		System.out.println(strObj);
+		
+		System.out.println(myPhone);
+	}
+}

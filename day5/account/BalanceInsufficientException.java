9f1fc4df19b8eee95ba79ba9f6932344710f5757
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-05-30 18:37:56 +0900

    Delete day5 directory

--- day5/account/BalanceInsufficientException.java
+++
@@ -1,7 +0,0 @@
-package account;
-public class BalanceInsufficientException extends Exception {
-	public BalanceInsufficientException() { }
-	public BalanceInsufficientException(String message) {
-		super(message);
-	}
-}
